6ac7c2ca230d20afb8ad3ea36d4c1a507f1f0f5a
bujosa <davidbujosa@gmail.com>
2023-08-27 21:26:27 -0400

    feat: init avatar page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
+import Avatar from './pages/Avatar';
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Chat/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/avatar" element={<Avatar />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
