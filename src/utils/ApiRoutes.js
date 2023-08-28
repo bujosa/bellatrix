067e5a711fc85677e86d68ac39f8925dea9d6a8d
bujosa <davidbujosa@gmail.com>
2023-08-27 22:04:30 -0400

    feat: add api users avatar

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -2,3 +2,4 @@ const host = process.env.REACT_APP_API_HOST;
 
 export const registerRoute = `${host}/api/users/register`;
 export const loginRoute = `${host}/api/users/login`;
+export const avatarRoute = `${host}/api/users/avatar`;
