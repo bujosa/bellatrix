46b2f40179e0cb997ec3092708bc4427b5a4f7aa
bujosa <davidbujosa@gmail.com>
2023-08-28 22:16:48 -0400

    style: organized by alphabet

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -1,5 +1,5 @@
 const host = process.env.REACT_APP_API_HOST;
 
-export const registerRoute = `${host}/api/users/register`;
-export const loginRoute = `${host}/api/users/login`;
 export const avatarRoute = `${host}/api/users/avatar`;
+export const loginRoute = `${host}/api/users/login`;
+export const registerRoute = `${host}/api/users/register`;
