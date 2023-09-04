97632742440a3c7c4b29a1c3a60d2ba49375f3e9
bujosa <davidbujosa@gmail.com>
2023-09-03 22:23:07 -0400

    feat: add states for avatar page

--- src/pages/Avatar.jsx
+++ src/pages/Avatar.jsx
@@ -4,14 +4,33 @@ import styled from 'styled-components';
 import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
+import { avatarRoute } from '../utils/ApiRoutes';
 
 export default function Avatar() {
   const api =
     'https://api.multiavatar.com/username.svg?size=128&background=none&color=fff&length=1&bold=true&rounded=true&fon';
-  const [avatar, setAvatar] = React.useState('');
+  const [avatar, setAvatar] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedAvatar, setSelectedAvatar] = useState(undefined);
   const navigate = useNavigate();
 
-  return <div>Avatar</div>;
+  return (
+    <>
+      <Container>
+        <div className="title-container">
+          <h1>Avatar Profile Picture</h1>
+        </div>
+        <div className="avatars">
+          {avatar ? (
+            <img src={avatar} alt="avatar" />
+          ) : (
+            <img src={api} alt="avatar" />
+          )}
+        </div>
+      </Container>
+      <ToastContainer />
+    </>
+  );
 }
 
 const Container = styled.div``;
