99328799559c4016352c9ca4980a321b2691bd43
bujosa <davidbujosa@gmail.com>
2023-09-03 21:29:47 -0400

    feat: add neccesary import like toast, axios for avatar page

--- src/pages/Avatar.jsx
+++ src/pages/Avatar.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
 
 export default function Avatar() {
   const api =
     'https://api.multiavatar.com/username.svg?size=128&background=none&color=fff&length=1&bold=true&rounded=true&fon';
+  const [avatar, setAvatar] = React.useState('');
+  const navigate = useNavigate();
 
   return <div>Avatar</div>;
 }
