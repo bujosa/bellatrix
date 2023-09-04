d62b3dba71d400801e7ae0df77a7e25286c44e83
bujosa <davidbujosa@gmail.com>
2023-09-03 21:26:14 -0400

    style: add axios, styled and container div for avatar

--- src/pages/Avatar.jsx
+++ src/pages/Avatar.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
 export default function Avatar() {
+  const api =
+    'https://api.multiavatar.com/username.svg?size=128&background=none&color=fff&length=1&bold=true&rounded=true&fon';
+
   return <div>Avatar</div>;
 }
+
+const Container = styled.div``;
